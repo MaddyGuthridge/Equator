7672ec130fa688365f2f2e524d94b70a6346abf2
Miguel Guthridge <hdsq@outlook.com.au>
2021-06-21 19:51:43 +1000

    Convert results to fractions when solving

--- lib/solve.py
+++ lib/solve.py
@@ -1,5 +1,7 @@
 import sympy as sym
 
+from fractions import Fraction
+
 from . import parse
 from . import segment
 
@@ -15,7 +17,14 @@ def solve(inp: str):
 
     res = sym.solve(parsed)
     
-    if isinstance(res, list):
-        return res
-    else:
-        return [res]
+    if not isinstance(res, list):
+        res = [res]
+
+    # Change to fractions if possible
+    for r in res:
+        for key, value in r.items():
+            v = Fraction(str(value))
+            if len(str(v)) < 10:
+                r[key] = str(v)
+
+    return res
