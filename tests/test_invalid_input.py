2e40ad701c39f520a931201b8be8703919634e0a
Miguel Guthridge <hdsq@outlook.com.au>
2021-08-20 17:32:34 +1000

    ✏CHANGE: Fix pytest

--- tests/test_invalid_input.py
+++ tests/test_invalid_input.py
@@ -3,26 +3,28 @@
 
 import pytest
 
+from ..lib.eq_except import *
+
 from .helpers import equate
 
 def test_bad_symbols_dot():
-    with pytest.raises(ValueError):
+    with pytest.raises(EqTokeniseException):
         equate("ev.di")
 
 def test_bad_symbols_leading_number():
-    with pytest.raises(ValueError):
+    with pytest.raises(EqTokeniseException):
         equate("12vjr")
 
 def test_bad_exponent_notation_space():
-    with pytest.raises(ValueError):
+    with pytest.raises(EqTokeniseException):
         equate("1e + 4")
-    with pytest.raises(ValueError):
+    with pytest.raises(EqTokeniseException):
         equate("1 e4")
 
 def test_bad_exponent_notation_bad_exp():
-    with pytest.raises(ValueError):
+    with pytest.raises(EqTokeniseException):
         equate("1e3.5")
 
 def test_bad_symbols_after_semicolon():
-    with pytest.raises(ValueError):
+    with pytest.raises(EqTokeniseException):
         equate("1 + 1; 1r5i")
