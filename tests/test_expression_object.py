5aad44b331741d7c0bfd015ee40aa8e5695134f4
Miguel Guthridge <hdsq@outlook.com.au>
2021-08-20 18:57:11 +1000

    🚀TESTS: Add tests to improve coverage for SubExpression object

---
+++ tests/test_expression_object.py
@@ -0,0 +1,52 @@
+"""Ensure that expressions have full code coverage
+"""
+
+from ..lib import Expression
+from ..lib.tokens import Number, Constant, Symbol, Operator
+from .helpers import doOneSolutionEq, doManySolutionEq, equate
+
+def test_stringify():
+    e = Expression("20 + 22")
+    assert(e.getOutputStr() == "42")
+
+def test_stringify_equation():
+    e = Expression("20 + 22 = x")
+    assert(e.getOutputStr() == "x = 42")
+
+def test_stringify_mixed():
+    e = Expression("20 + 22 = x; x + 27")
+    assert(e.getOutputStr() == "x = 42\n69")
+
+def test_stringify_multi_solution():
+    e = Expression("x^2 + 2*x = 3")
+    assert(e.getOutputStr() == 
+"""[1]:
+\tx = -3
+[2]:
+\tx = 1""")
+
+def test_get_input_tokens():
+    e = Expression("2 + pi - x")
+    tokens = e.getInputTokens()
+    exp = ([[
+         Number("2"),
+         Operator("+"),
+         Constant("pi"),
+         Operator("-"),
+         Symbol("x")
+     ]],
+     "")
+    assert(tokens == exp)
+
+def test_get_output_tokens():
+    e = Expression("20 + 22")
+    exp = [([], [[
+         Number("42")
+     ]])]
+    tokens = e.getOutputTokens()
+    assert(tokens == exp)
+
+def test_cache():
+    e = Expression("42")
+    assert(e.getOutputStr() == "42")
+    assert(e.getOutputStr() == "42")
