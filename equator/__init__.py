b49822e22729b28acb8217d34b8d357644f2488a
Miguel Guthridge <hdsq@outlook.com.au>
2021-08-31 16:25:04 +1000

    ✨NEW: Create __init__.py for module

---
+++ equator/__init__.py
@@ -0,0 +1,12 @@
+"""Equator
+
+An advanced symbolic calculator built using SymPy
+
+This project is licensed under the GNU General Public License v3.0
+
+Author: Miguel Guthridge
+"""
+
+from .lib import equate, Expression
+from .lib.tokens import Token, Operator, Number, Constant, Symbol, BadToken
+from .lib.eq_except import *
