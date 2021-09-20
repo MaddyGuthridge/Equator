ca139bab8f85ffdac61e54be777d1e130b8b41cf
Miguel Guthridge <hdsq@outlook.com.au>
2021-09-20 22:17:49 +1000

    🚀TESTS: Add tests for missing elements within an ArgSet

--- tests/test_comma_operator.py
+++ tests/test_comma_operator.py
@@ -7,7 +7,7 @@ import pytest
 
 from .helpers import equate
 
-from equator import EqCommaError
+from equator import EqCommaError, EqParserException
 
 def testCollect():
     """Collect comma-separated items. Raise exception because must be sent to
@@ -21,3 +21,21 @@ def testComplexCollect():
     """
     with pytest.raises(EqCommaError):
         equate("2 * (2, 5, x) - 4 + (2 * 3, 1, 3 / 2)")
+
+def testMissingBefore():
+    """Missing item before a comma
+    """
+    with pytest.raises(EqParserException):
+        equate("max(, 2, 3, 4)")
+
+def testMissingAfter():
+    """Missing item after a comma
+    """
+    with pytest.raises(EqParserException):
+        equate("max(2, 3, 4, )")
+
+def testMissingMiddle():
+    """Missing item between two commas
+    """
+    with pytest.raises(EqParserException):
+        equate("max(2, 3, 4, , 6)")
