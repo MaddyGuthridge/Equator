3023b8e04f12ead646ab6c366306e11a24fcc240
Miguel Guthridge <hdsq@outlook.com.au>
2021-09-22 19:21:24 +1000

    ✨NEW: Tests for GCD and LCM functions

---
+++ tests/test_functions/test_lcm_gcd_functions.py
@@ -0,0 +1,39 @@
+"""Check trig functions work correctly
+"""
+
+import pytest
+
+from ..helpers import doOneSolutionExp
+
+from equator import EqFunctionArgumentException
+
+def test_lcm_simple():
+    assert doOneSolutionExp("lcm(2, 3)") == ["6"]
+
+def test_lcm_symbol():
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("lcm(x, 1)")
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("lcm(1, x)")
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("lcm(x, y)")
+
+def test_lcm_decimal():
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("lcm(1.5, 2)")
+
+def test_gcd_simple():
+    assert doOneSolutionExp("gcd(8, 12)") == ["4"]
+    assert doOneSolutionExp("gcd(48, 18)") == ["6"]
+
+def test_gcd_symbol():
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("gcd(x, 1)")
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("gcd(1, x)")
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("gcd(x, y)")
+
+def test_gcd_decimal():
+    with pytest.raises(EqFunctionArgumentException):
+        doOneSolutionExp("gcd(1.5, 2)")
