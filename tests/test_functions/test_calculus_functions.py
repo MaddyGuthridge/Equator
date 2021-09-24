ba369d06cd9205a81f5f49894494901a3d5066d8
Miguel Guthridge <hdsq@outlook.com.au>
2021-09-25 01:21:58 +1000

    🛠CHANGE: Fix calculus tests

--- tests/test_functions/test_calculus_functions.py
+++ tests/test_functions/test_calculus_functions.py
@@ -3,46 +3,46 @@
 
 import pytest
 
-from ..helpers import doOneSolutionEq
+from ..helpers import doOneSolutionExp
 
 from equator import EqFunctionArgumentException
 
 def test_differentiate_contant():
-    assert doOneSolutionEq("diff(42, x)") == ["0"]
+    assert doOneSolutionExp("diff(42, x)") == ["0"]
 
 def test_differentiate_simple():
-    assert doOneSolutionEq("diff(2 * x^2 - 5 * x + 4, x)") == ["4*x - 5"]
+    assert doOneSolutionExp("diff(2 * x^2 - 5 * x + 4, x)") == ["4*x^1-5"]
 
 def test_differentiate_complex():
-    assert doOneSolutionEq("diff(sin(x) - cos(x)^2 + 2*ln(2 * x), x)") == ["?"]
+    assert doOneSolutionExp("diff(sin(x) - cos(x)^2 + 2*ln(2 * x), x)") == ["((2*sin(x))*cos(x)^1+cos(x))+2/x"]
 
 def test_differentiate_other_symbols():
-    assert doOneSolutionEq("diff(2 * x^2 - 5 * x + a, x)") == ["4*x - 5 + da/dx"]
+    assert doOneSolutionExp("diff(2 * x^2 - 5 * x + a, x)") == ["4*x^1-5"]
 
 def test_differentiate_invalid_symbol():
     with pytest.raises(EqFunctionArgumentException):
-        doOneSolutionEq("diff(2 * x, 1)")
+        doOneSolutionExp("diff(2 * x, 1)")
 
 ################################################################################
 
 def test_integrate_constant():
-    assert doOneSolutionEq("int(42, x)") == ["42 * x"]
+    assert doOneSolutionExp("int(42, x)") == ["42*x"]
 
 def test_integrate_simple():
-    assert doOneSolutionEq("int(2 * x - 5, x)") == ["x^2 - 5*x"]
+    assert doOneSolutionExp("int(2 * x - 5, x)") == ["x*(1*x-5)"]
 
 def test_integrate_complex():
-    assert doOneSolutionEq("int(sin(x) - cos(x)^2 + 2*ln(2 * x), x)") == ["?"]
+    assert doOneSolutionExp("int(sin(x) - cos(x)^2 + 2*ln(2 * x), x)") == ["?"]
 
 def test_integrate_other_symbols():
-    assert doOneSolutionEq("int(6 * x + a, x)") == ["?"]
+    assert doOneSolutionExp("int(6 * x + a, x)") == ["x*(1*a+3*x)"]
 
 def test_integrate_invalid_symbol():
     with pytest.raises(EqFunctionArgumentException):
-        doOneSolutionEq("int(2 * x, 1)")
+        doOneSolutionExp("int(2 * x, 1)")
 
 def test_integrate_range():
-    assert doOneSolutionEq("int(42, x = 1, 5)") == ["210"] # ?
+    assert doOneSolutionExp("int(42, x = 1, 5)") == ["210"] # ?
 
 def test_integrate_invalid_range():
     # Is this even possible?
