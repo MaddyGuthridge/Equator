f063cca334772b6be7cfbc2781821facdbe4c51d
Miguel Guthridge <hdsq@outlook.com.au>
2021-09-27 16:44:11 +1000

    🚀TESTS: Fix dodgy tests for integration

--- tests/test_functions/test_calculus_functions.py
+++ tests/test_functions/test_calculus_functions.py
@@ -29,17 +29,17 @@ def test_integrate_constant():
     assert doOneSolutionExp("int(42, x)") == ["42*x"]
 
 def test_integrate_simple():
-    assert doOneSolutionExp("int(2 * x - 5, x)") == ["x*(1*x-5)"]
+    assert doOneSolutionExp("int(2 * x - 5, x)") == ["x*(x-5)"]
 
 def test_integrate_complex():
     assert doOneSolutionExp("int(sin(x) - cos(x) + 2*ln(2 * x), x)")\
-        == ["((2*x)*log(x)-0.613705638880109*x)-(1*sqrt(2))*sin(x+pi/4)"]
+        == ["(((2*x)*log(x)-2*x)+x*log(4))-sqrt(2)*sin(x+pi/4)"]
 
-# def test_integrate_square_of_trig():
-#     assert doOneSolutionExp("int(cos(x)^2, x)") == ["x/2+sin(x)*cos(x)/2"]
+def test_integrate_square_of_trig():
+    assert doOneSolutionExp("int(cos(x)^2, x)") == ["x/2+sin(2*x)/4"]
 
 def test_integrate_other_symbols():
-    assert doOneSolutionExp("int(6 * x + a, x)") == ["x*(1*a+3*x)"]
+    assert doOneSolutionExp("int(6 * x + a, x)") == ["x*(a+3*x)"]
 
 def test_integrate_invalid_symbol():
     with pytest.raises(EqFunctionArgumentException):
