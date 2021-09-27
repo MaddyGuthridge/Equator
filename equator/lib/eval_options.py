98d8fe5b5c441d8fe6df3e376520b150e3ec13ea
Miguel Guthridge <hdsq@outlook.com.au>
2021-09-27 16:41:25 +1000

    ✏CHANGE: Improvements to evalation options

--- equator/lib/eval_options.py
+++ equator/lib/eval_options.py
@@ -8,11 +8,18 @@ integers.
 class EvalOptions:
     """Options for how evaluations should be done
     """
-    def __init__(self, number_as_rational:bool=False) -> None:
+    def __init__(self, previous_options:'EvalOptions'=None, number_as_rational:bool=None) -> None:
         """Create a set of evaluation options
 
         Args:
             number_as_rational (bool, optional): Whether to force SymPy rational
-            types for evaluation. Defaults to False.
+            types for evaluation. Defaults to None.
         """
-        self.number_as_rational = number_as_rational
+        
+        # FIXME: This is horribly yucky
+        self.number_as_rational = number_as_rational\
+            if number_as_rational is not None\
+            else (previous_options.number_as_rational
+                if previous_options is not None
+                else False
+                  )
